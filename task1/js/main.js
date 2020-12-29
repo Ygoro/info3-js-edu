306d23dd47b5016eb3c11e52fa167a3d78cc936c
ikosmerl <igor.kosmerl@convergent-usa.com>
2020-12-29 09:59:56 +0100

    Finished task1 functionalities, needs adaptation of cell size on mobile

--- task1/js/main.js
+++ task1/js/main.js
@@ -1,14 +1,15 @@
-'use strict';
+"use strict";
 
+// User choice object with default values
 var userChoice = {
     gridSize: 16,
-    cellColor: '#ffffff'
-}
+    cellColor: "#ffffff",
+};
 
 // Generates random hex color
 function getRandomColor() {
-    const hexChars = '0123456789ABCDEF';
-    let color = '#';
+    const hexChars = "0123456789ABCDEF";
+    let color = "#";
 
     for (let i = 0; i < 6; i++) {
         color += hexChars[Math.floor(Math.random() * 16)];
@@ -17,63 +18,75 @@ function getRandomColor() {
     return color;
 }
 
-// Sets randomly generated color
-function setRandomColor() {
-    $('#color').val(getRandomColor());
-}
-
-// Fills cell with random color on hover
-function drawRandomColor() {
-    $('cell').hover(function() {
-        row.css('backgroundColor', getRandomColor());
-    });
-}
-
 // Generates blank table of defined size
 function generateTable(size) {
-    let table = $('<table>').addClass('grid-table');
+    let table = $("<table>").addClass("grid-table");
 
     for (let i = 0; i < size; i++) {
-        let column = $('<tr>');
+        let column = $("<tr>");
         for (let j = 0; j < size; j++) {
-            let row = $('<td></td>').addClass('cell');
+            let row = $("<td></td>").addClass("cell");
             column.append(row);
         }
 
-        column.append('</tr>');
+        column.append("</tr>");
         table.append(column);
     }
-    table.append('</table>');
+    table.append("</table>");
 
-    $('.drawing-panel').append(table);
+    $(".drawing-panel").append(table);
 }
 
-function determineColor() {
+$(document).ready(function () {
+    $("#reset").hide();
+    $("#color").hide();
 
-}
+    // Generate button event
+    $("#generate").on("click", function () {
+        userChoice.gridSize = $("#grid").val();
 
-$(document).ready(function() {
-    $('#reset').hide();
+        if (userChoice.gridSize >= 16 && userChoice.gridSize <= 64) {
+            $("#generate").prop("disabled", false);
+            $(".controls").hide();
+            $("#generate").hide();
+            $("#reset").show();
 
-    $('#generate').on('click', function() {
-        $('.controls').hide();
-        $('#generate').hide();
-        $('#reset').show();
+            generateTable(userChoice.gridSize);
 
-        userChoice.gridSize = $('#grid').val();
-        userChoice.cellColor = $('#color').val();
-
-        generateTable(userChoice.gridSize);
+            if ($("#color-needed").is(":checked")) {
+                userChoice.cellColor = $("#color").val();
+                $(".cell").hover(function () {
+                    $(this).css("backgroundColor", userChoice.cellColor);
+                });
+            } else {
+                userChoice.cellColor = getRandomColor();
+                $(".cell").each(function () {
+                    $(this).hover(function () {
+                        $(this).css("backgroundColor", getRandomColor());
+                    });
+                });
+            }
+        } else {
+            alert("Grid size must be between 16 and 64");
+        }
     });
 
-    $('.cell').mouseenter(function() {
-        $(this).css('backgroundColor', userChoice.cellColor);
-    })
-
-    $('#reset').on('click', function() {
+    // Reset button event
+    $("#reset").on("click", function () {
         location.reload();
-        $('.controls').show();
-        $('#generate').show();
-        $('.drawing-panel').hide();
+        $(".controls").show();
+        $("#generate").show();
+        $(".drawing-panel").hide();
+    });
+
+    // Hide/show color selection content
+    $("#color-needed").on("click", function () {
+        if ($(this).is(":checked")) {
+            $(".random").hide();
+            $("#color").show();
+        } else {
+            $(".random").show();
+            $("#color").hide();
+        }
     });
 });
